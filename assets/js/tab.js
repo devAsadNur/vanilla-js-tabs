365067e3db9b14fd5697d5b1517617d63a447392
Cyber Asad <cyber.asad24@gmail.com>
2020-06-02 01:27:59 +0600

    Custom data attribute added instead of different class names. Also, changed the js mechanism to dataset

--- assets/js/tab.js
+++ assets/js/tab.js
@@ -8,29 +8,26 @@ tabSelectors.forEach((selector, index, tabSelectors) => {
     // Run event listener for each tab selector
     selector.addEventListener('click', function (e) {
 
-        // Class list of each tab selector
-        let taretElementClasses = e.target.classList;
+       let selectedTabNum = e.target.dataset.tab;  // Selected tab number
 
-        // Looping through each class name of each tab selector
-        taretElementClasses.forEach(singleClass => {
+        // Looping through each of the tab selectors
+        tabSelectors.forEach(item => {
 
             // If 'tab selector' not equel to 'triggered tab', then remove 'active' class 
             // Else add 'active' class to the 'tab selector'
-            if (singleClass !== `tab-item-${index+1}`) {
-                tabSelectors.forEach(tabSelector => {
-                    tabSelector.classList.remove('active');
-                });
+            if (item.dataset.tab !== selectedTabNum) {
+                item.classList.remove('active');
             } else {
                 this.classList.add('active');
             }
-        });
+        })
 
-        // Looping through each tab contents
-        tabContents.forEach((content) => {
+        // Looping through each of the tab contents
+        tabContents.forEach(content => {
 
-            // If 'tab content' not equel to 'selected tab', then remove 'active' class 
+            // If 'tab content' not equel to 'selected tab', then remove 'active' class
             // Else add 'active' class to the 'tab content'
-            if (!content.classList.contains(`tab-content-${index+1}`)) {
+            if (content.dataset.tab !== selectedTabNum) {
                 content.classList.remove('active');
             } else {
                 content.classList.add('active');
