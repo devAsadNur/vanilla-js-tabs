329bebb235af96825a7e8e122bc61ce1ce946a85
Cyber Asad <cyber.asad24@gmail.com>
2020-06-02 18:45:00 +0600

    Added support for multiple tabs in a single HTML page

--- assets/js/tab.js
+++ assets/js/tab.js
@@ -1,39 +1,45 @@
 // Scripts for Tab Start
-let tabSelectors = document.querySelectorAll('.tab-item'); // Tab Selectors
-let tabContents = document.querySelectorAll('.tab-content'); // Tab Contents
+let tabWrappers = document.querySelectorAll('.tab-wrapper'); // Tab Selectors
 
-// Looping through each tab selector
-tabSelectors.forEach((selector, index, tabSelectors) => {
+// Looping through each of the tab wrappers
+tabWrappers.forEach(wrapper => {
+    let tabSelectors = wrapper.querySelectorAll('.tab-item'); // Tab Selectors
+    let tabContents = wrapper.querySelectorAll('.tab-content'); // Tab Contents
 
-    // Run event listener for each tab selector
-    selector.addEventListener('click', function (e) {
+    // Looping through each tab selector
+    tabSelectors.forEach((selector, index, tabSelectors) => {
 
-       let selectedTabNum = e.target.dataset.tab;  // Selected tab number
+        // Run event listener for each tab selector
+        selector.addEventListener('click', function (e) {
 
-        // Looping through each of the tab selectors
-        tabSelectors.forEach(item => {
+            let selectedTabNum = e.target.dataset.tab; // Selected tab number
 
-            // If 'tab selector' not equel to 'triggered tab', then remove 'active' class 
-            // Else add 'active' class to the 'tab selector'
-            if (item.dataset.tab !== selectedTabNum) {
-                item.classList.remove('active');
-            } else {
-                this.classList.add('active');
-            }
-        })
+            // Looping through each of the tab selectors
+            tabSelectors.forEach(item => {
 
-        // Looping through each of the tab contents
-        tabContents.forEach(content => {
+                // If 'tab selector' not equel to 'triggered tab', then remove 'active' class 
+                // Else add 'active' class to the 'tab selector'
+                if(item.dataset.tab !== selectedTabNum) {
+                    item.classList.remove('active');
+                } else {
+                    this.classList.add('active');
+                }
+            });
 
-            // If 'tab content' not equel to 'selected tab', then remove 'active' class
-            // Else add 'active' class to the 'tab content'
-            if (content.dataset.tab !== selectedTabNum) {
-                content.classList.remove('active');
-            } else {
-                content.classList.add('active');
-            }
+            // Looping through each of the tab contents
+            tabContents.forEach(content => {
+
+                // If 'tab content' not equel to 'selected tab', then remove 'active' class
+                // Else add 'active' class to the 'tab content'
+                if(content.dataset.tab !== selectedTabNum) {
+                    content.classList.remove('active');
+                } else {
+                    content.classList.add('active');
+                }
+            });
+
         });
-
     });
+
 });
 // Scripts for Tab End
